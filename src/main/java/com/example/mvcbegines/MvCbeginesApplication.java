51fa083b884ade8b0c4b4430cd247be822ec2115
TMAdevby <mtipun@mail.ru>
2026-08-25 20:28:26 +0300

    first commit

---
+++ src/main/java/com/example/mvcbegines/MvCbeginesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcbegines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCbeginesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvCbeginesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvcbegines/MvCbeginesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcbegines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCbeginesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
